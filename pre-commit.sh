cd96e945b0dd0338ee389704b13ff74123d4d51c
Daniele Ambrosino <mail@danieleambrosino.it>
2024-11-09 17:03:56 +0100

    reorganize

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Generating indexes..."
-ls sheets | grep 'abc$' > sheets/index.txt
 ls appunti | grep 'md$' > appunti/index.txt
+ls spartiti | grep 'abc$' > spartiti/index.txt
 echo "Staging index files..."
-git add sheets/index.txt
+git add appunti/index.txt spartiti/index.txt
