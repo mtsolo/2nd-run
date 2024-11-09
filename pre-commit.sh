9563428007fefd932e9b6e847d74404dbd4637af
Daniele Ambrosino <mail@danieleambrosino.it>
2024-11-09 16:15:10 +0100

    2024-11-09T16:15+01:00

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 
-NO_FORMAT="\033[0m"
-F_BOLD="\033[1m"
-C_GREEN="\033[38;5;2m"
-
-echo -e "${F_BOLD}${C_GREEN}Running pre-commit hook...${NO_FORMAT}"
 echo "Generating sheets index..."
 ls sheets | grep 'abc$' > sheets/index.txt
-echo "Staging file..."
+echo "Staging index file..."
 git add sheets/index.txt
-echo -e "${F_BOLD}${C_GREEN}Pre-commit hook done!${NO_FORMAT}"
